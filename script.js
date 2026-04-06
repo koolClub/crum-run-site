fd9c5fdd6532a19475c42b11265027ce7e3eb41d
koolClub <superdavematheson@gmail.com>
2026-04-06 01:14:57 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const toggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.site-nav');
-
-if (toggle && nav) {
-  toggle.addEventListener('click', () => {
-    const open = nav.classList.toggle('open');
-    toggle.setAttribute('aria-expanded', String(open));
-  });
-}
